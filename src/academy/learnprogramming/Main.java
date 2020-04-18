4d30d49764f41576c1d9d49ffc611745495f9bc0
ryoray <praretno.wibowo@gmail.com>
2020-04-19 06:19:08 +0700

    Add a constant to replace redundant invalid message

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -2,15 +2,17 @@ package academy.learnprogramming;
 
 public class Main {
 
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
+
     public static void main(String[] args) {
 
-        System.out.println(getDurationString(65L, 45L));
+        System.out.println(getDurationString(-65L, 45L));
         System.out.println(getDurationString(3945L));
     }
 
     private static String getDurationString (long minutes, long seconds) {
         if ((minutes < 0) || (seconds < 0 || seconds > 59)) {
-            return "Invalid Value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         long hours = (minutes / 60);
@@ -36,7 +38,7 @@ public class Main {
 
     private static String getDurationString (long seconds) {
         if (seconds < 0) {
-            return "Invalid Value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         long minutes = seconds / 60;

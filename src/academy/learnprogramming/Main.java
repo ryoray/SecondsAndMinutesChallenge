c29ab1a0b9f2f63c76feeb5bf9d814ca3a01af9c
ryoray <praretno.wibowo@gmail.com>
2020-04-18 15:24:59 +0700

    final working code

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -4,16 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        getDurationString(135,27);
+        System.out.println(getDurationString(65L, 45L));
+        System.out.println(getDurationString(3945L));
     }
 
-    public static int getDurationString (int minutes, int seconds) {
+    private static String getDurationString (long minutes, long seconds) {
         if ((minutes < 0) || (seconds < 0 || seconds > 59)) {
-            return -1;
+            return "Invalid Value";
         }
-        int hours = (minutes / 60);
-        minutes = (minutes % 60);
-        System.out.println(hours + "h " + minutes + "m " + seconds + "s");
-        return hours;
+
+        long hours = (minutes / 60);
+        long remainingMinutes = (minutes % 60);
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+    }
+
+    private static String getDurationString (long seconds) {
+        if (seconds < 0) {
+            return "Invalid Value";
+        }
+
+        long minutes = seconds / 60;
+        long remainingSeconds = seconds % 60;
+
+        return getDurationString(minutes, remainingSeconds);
     }
 }
